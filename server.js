5e671150728f301ac4852b9a4958a7b66fb0bada
NowyMe <nowy.meepho@hva.nl>
2022-05-30 15:35:32 +0200

    api

--- server.js
+++ server.js
@@ -95,3 +95,5 @@ app.use((req, res) => {
 
 
 
+
+
--- static/script.js
+++ static/script.js
@@ -1,19 +1,46 @@
+// const encodedParams = new URLSearchParams();
+// encodedParams.append("requestUrl", "<REQUIRED>");
+// encodedParams.append("userKey", "<REQUIRED>");
+// encodedParams.append("pageIds", "<REQUIRED>");
+
+// const options = {
+// 	method: 'POST',
+// 	headers: {
+// 		'content-type': 'application/x-www-form-urlencoded',
+// 		'X-RapidAPI-Host': 'GameDatabasestefan-skliarovV1.p.rapidapi.com',
+// 		'X-RapidAPI-Key': '4a7a27e753mshe8c1eed5f04fb1dp1b9763jsna989b692cae0'
+// 	},
+// 	body: encodedParams
+// };
+
+// fetch('https://gamedatabasestefan-skliarovv1.p.rapidapi.com/getPages', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log('test',response))
+// 	.catch(err => console.error(err));
+    
+
+//     console.log('etetetetet')
+
+
+
 const encodedParams = new URLSearchParams();
 encodedParams.append("requestUrl", "<REQUIRED>");
 encodedParams.append("userKey", "<REQUIRED>");
 encodedParams.append("pageIds", "<REQUIRED>");
 
 const options = {
+    mode: 'no-cors',
 	method: 'POST',
 	headers: {
-		'content-type': 'application/x-www-form-urlencoded',
-		'X-RapidAPI-Host': 'GameDatabasestefan-skliarovV1.p.rapidapi.com',
-		'X-RapidAPI-Key': '4a7a27e753mshe8c1eed5f04fb1dp1b9763jsna989b692cae0'
-	},
-	body: encodedParams
+		'Client-ID': 'vbwxbmene4l1wgm17cfr5c9b60xd9r',
+		'Authorization': 'Bearer samjeh51ql0q240la094i1pwmakt11',
+		'Content-Type': 'application/json'
+	}
+	// body: 
+    // "fields: *; where id = 110592;"
 };
 
-fetch('https://gamedatabasestefan-skliarovv1.p.rapidapi.com/getPages', options)
+fetch('https://api.igdb.com/v4/covers/', options)
 	.then(response => response.json())
 	.then(response => console.log('test',response))
 	.catch(err => console.error(err));
