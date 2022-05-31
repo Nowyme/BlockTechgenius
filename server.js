41a4ccd90a2c46440caa56801b2e62bd8aca90ee
NowyMe <nowy.meepho@hva.nl>
2022-06-01 00:19:28 +0200

    unlike

--- server.js
+++ server.js
@@ -1,36 +1,42 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
+require("dotenv").config();
 
-require('dotenv').config();
-
-const express = require('express');
-const gameImg = require('./data/games');
-
-
-const req = require('express/lib/request');
-const res = require('express/lib/response');
-
+const express = require("express");
+const req = require("express/lib/request");
+const res = require("express/lib/response");
 
 // express app
 const app = express();
-
-app.use(express.static('static'));
+app.use(express.static("static"));
 
 // register view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-
-// mongoDB
+// mongoDB connect
 let db = null;
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const { ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const { ObjectId } = require("mongodb");
 
 async function connectDB() {
-  const uri = 'mongodb+srv://' + process.env.DB_USERNAME + ':' + process.env.DB_PASS + '@' + process.env.DB_HOST + '/' + process.env.DB_NAME + '?retryWrites=true&w=majority';
-  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+  const uri =
+    "mongodb+srv://" +
+    process.env.DB_USERNAME +
+    ":" +
+    process.env.DB_PASS +
+    "@" +
+    process.env.DB_HOST +
+    "/" +
+    process.env.DB_NAME +
+    "?retryWrites=true&w=majority";
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+  });
 
   try {
     await client.connect();
@@ -39,77 +45,100 @@ async function connectDB() {
     console.log(error);
     throw error;
   }
-
 }
 
 // start webserver
-
 app.listen(process.env.PORT, () => {
   console.log(`Server running on http://localhost:${process.env.PORT}`);
 
   connectDB().then(console.log("connectie met mongodb"));
-  
 });
 
+// index
+app.get("/", async (req, res) => {
+  try {
+    const games = await db.collection("game_collection").find({}, {}).toArray();
+    const user = await db.collection("user").find({}, {}).toArray();
 
- app.get('/', async (req, res) => {
-
-  const games = await db.collection("game_collection").find({}, {}).toArray();
-  
-  const title = (games.length == 0) ? "No games where found" : "Games";
-  res.render('pages/index', { title, games });
-  
+    res.render("pages/index", { games, user });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
+// mygames
+app.get("/mygames", async (req, res) => {
+  try {
+    const likegames = await db
+      .collection("game_collection")
+      .find({ like: true }, {})
+      .toArray();
+    const user = await db.collection("user").find({}, {}).toArray();
+    res.render("pages/mygames", { likegames, user });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
+// like
+app.post("/", async (req, res) => {
+  try {
+    const games = db.collection("game_collection");
 
-app.post('/', async (req, res) => {
-  try{
-  const games = db.collection('game_collection');
-  
-  console.log(games);
+    const users = await db.collection("user");
 
-    await games.updateOne({ _id: ObjectId(req.body.like) }, { $set: { like: true } });
-    
-    res.redirect('pages/index');
+    await games.updateOne(
+      { _id: ObjectId(req.body.like) },
+      { $set: { like: true } }
+    );
+
+    await users.updateOne(
+      { _id: ObjectId("62967307a4ff59e9678d2943") },
+      { $push: { game_id: req.body.like } }
+    );
+
+    res.redirect("/");
   } catch (err) {
     console.log(err);
   }
-
-  
 });
 
+// unlike
+app.post("/unlike", async (req, res) => {
+  try {
+    const games = db.collection("game_collection");
 
+    const users = await db.collection("user");
 
+    await games.updateOne(
+      { _id: ObjectId(req.body.unlike) },
+      { $set: { like: false } }
+    );
 
+    await users.updateOne(
+      { _id: ObjectId("62967307a4ff59e9678d2943") },
+      { $pull: { game_id: req.body.unlike } }
+    );
 
+    res.redirect("/mygames");
+  } catch (err) {
+    console.log(err);
+  }
+});
 
+//work in progress
+// mygames display 
+app.get("pages/mygames", async (req, res) => {
+  const user = await db.collection("user").findOne("62967307a4ff59e9678d2943");
+  const games = await db.collection("game_collection").find({}, {}).toArray();
+  console.log(user);
+  const userGames = games.filter((game) => user.game_id.includes(game.id));
+  console.log(userGames);
 
-
-
-
-
-//  const gamesSchema = {
-//    name: String,
-//    genre: String,
-//  }
-//  const Game = mongoose.model('Game',  gamesSchema);
-
-//  app.get('/'), (req, res) => {
-//    Game.find({}, function(err, game_collection){
-//      res.render('pages/index',{
-//        gamesList: games
-//      })
-//    })
-
-
-
+  res.render("pages/mygames", { user, games, userGames });
+});
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).render('pages/404');
+  res.status(404).render("pages/404");
 });
-
-
-
-
--- static/script.js
+++ static/script.js
@@ -1,28 +1,3 @@
-// const encodedParams = new URLSearchParams();
-// encodedParams.append("requestUrl", "<REQUIRED>");
-// encodedParams.append("userKey", "<REQUIRED>");
-// encodedParams.append("pageIds", "<REQUIRED>");
-
-// const options = {
-// 	method: 'POST',
-// 	headers: {
-// 		'content-type': 'application/x-www-form-urlencoded',
-// 		'X-RapidAPI-Host': 'GameDatabasestefan-skliarovV1.p.rapidapi.com',
-// 		'X-RapidAPI-Key': '4a7a27e753mshe8c1eed5f04fb1dp1b9763jsna989b692cae0'
-// 	},
-// 	body: encodedParams
-// };
-
-// fetch('https://gamedatabasestefan-skliarovv1.p.rapidapi.com/getPages', options)
-// 	.then(response => response.json())
-// 	.then(response => console.log('test',response))
-// 	.catch(err => console.error(err));
-    
-
-//     console.log('etetetetet')
-
-
-
 const encodedParams = new URLSearchParams();
 encodedParams.append("requestUrl", "<REQUIRED>");
 encodedParams.append("userKey", "<REQUIRED>");
@@ -36,7 +11,7 @@ const options = {
 		'Authorization': 'Bearer samjeh51ql0q240la094i1pwmakt11',
 		'Content-Type': 'application/json'
 	},
-	body: 
+	body: encodedParams
    
 };
 
@@ -46,4 +21,4 @@ fetch('https://api.igdb.com/v4/covers/', options)
 	.catch(err => console.error(err));
     
 
-    console.log('etetetetet');
+    
