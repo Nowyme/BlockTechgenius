1166336809b4453cbd890dd1259dd2f8476cae0a
NowyMe <nowy.meepho@hva.nl>
2022-06-04 16:09:34 +0200

    API

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 const express = require("express");
 const req = require("express/lib/request");
 const res = require("express/lib/response");
+const cors = require("cors");
 
 
 // express app
@@ -16,6 +17,8 @@ app.set("view engine", "ejs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
 
 // mongoDB connect
 let db = null;
--- static/script.js
+++ static/script.js
@@ -1,24 +1,45 @@
-const encodedParams = new URLSearchParams();
-encodedParams.append("requestUrl", "<REQUIRED>");
-encodedParams.append("userKey", "<REQUIRED>");
-encodedParams.append("pageIds", "<REQUIRED>");
+// const { entries } = require("lodash");
+
+// const encodedParams = new URLSearchParams();
+// encodedParams.append("requestUrl", "<REQUIRED>");
+// encodedParams.append("userKey", "<REQUIRED>");
+// encodedParams.append("pageIds", "<REQUIRED>");
+
+// const options = {
+//     mode: 'no-cors',
+// 	method: 'POST',
+// 	headers: {
+// 		'Client-ID': 'vbwxbmene4l1wgm17cfr5c9b60xd9r',
+// 		'Authorization': 'Bearer samjeh51ql0q240la094i1pwmakt11',
+// 		'Content-Type': 'application/json'
+// 	},
+// 	body: encodedParams
+
+// };
+
+// fetch('https://api.igdb.com/v4/covers/', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log('test',response))
+// 	.catch(err => console.error(err));
+
+
+// Intersection observer 
+const cards = document.querySelectorAll(".card-item");
 
 const options = {
-    mode: 'no-cors',
-	method: 'POST',
-	headers: {
-		'Client-ID': 'vbwxbmene4l1wgm17cfr5c9b60xd9r',
-		'Authorization': 'Bearer samjeh51ql0q240la094i1pwmakt11',
-		'Content-Type': 'application/json'
-	},
-	body: encodedParams
-   
+  root: null,
+  threshold: 0.4,
+  rootMargin: "0px",
 };
 
-fetch('https://api.igdb.com/v4/covers/', options)
-	.then(response => response.json())
-	.then(response => console.log('test',response))
-	.catch(err => console.error(err));
+const observer = new IntersectionObserver(function (entries, observer) {
+  entries.forEach((entry) => {
+    entry.target.classList.toggle("slide-top", entry.isIntersecting);
+  });
+}, options);
     
 
-    
+cards.forEach((card) => {
+  observer.observe(card);
+  card.classList.add("hide");
+});
