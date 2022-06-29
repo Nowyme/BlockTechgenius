1c588ca2e9e3c81a727aaaf3b1112e80700510df
NowyMe <nowy.meepho@hva.nl>
2022-06-29 13:11:04 +0200

    add liking

--- server.js
+++ server.js
@@ -1,40 +1,38 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
-const req = require("express/lib/request");
-const res = require("express/lib/response");
-
+const express = require('express');
+const { use } = require('express/lib/application');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
 
 // express app
 const app = express();
-app.use(express.static("static"));
+app.use(express.static('static'));
 
 // register view engine
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-
 // mongoDB connect
 let db = null;
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const { ObjectId } = require("mongodb");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const { ObjectId } = require('mongodb');
 
 async function connectDB() {
   const uri =
-    "mongodb+srv://" +
+    'mongodb+srv://' +
     process.env.DB_USERNAME +
-    ":" +
+    ':' +
     process.env.DB_PASS +
-    "@" +
+    '@' +
     process.env.DB_HOST +
-    "/" +
+    '/' +
     process.env.DB_NAME +
-    "?retryWrites=true&w=majority";
+    '?retryWrites=true&w=majority';
   const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -54,41 +52,41 @@ async function connectDB() {
 app.listen(process.env.PORT, () => {
   console.log(`Server running on http://localhost:${process.env.PORT}`);
 
-  connectDB().then(console.log("connectie met mongodb"));
+  connectDB().then(console.log('connectie met mongodb'));
 });
 
 // index
-app.get("/", async (req, res) => {
+app.get('/', async (req, res) => {
   try {
-    const games = await db.collection("game_collection").find({}, {}).toArray();
-    const user = await db.collection("user").find({}, {}).toArray();
+    const games = await db.collection('game_collection').find({}, {}).toArray();
+    const user = await db.collection('user').find({}, {}).toArray();
 
-    res.render("pages/index", { games, user });
+    res.render('pages/index', { games, user });
   } catch (error) {
     console.log(error);
   }
 });
 
 // mygames
-app.get("/mygames", async (req, res) => {
-  try {
-    const likegames = await db
-      .collection("game_collection")
-      .find({ like: true }, {})
-      .toArray();
-    const user = await db.collection("user").find({}, {}).toArray();
-    res.render("pages/mygames", { likegames, user });
-  } catch (error) {
-    console.log(error);
-  }
-});
+// app.get('/mygames', async (req, res) => {
+//   try {
+//     const likegames = await db
+//       .collection('game_collection')
+//       .find({ like: true }, {})
+//       .toArray();
+//     const user = await db.collection('user').find({}, {}).toArray();
+//     res.render('pages/mygames', { likegames, user });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
 
 // like
-app.post("/", async (req, res) => {
+app.post('/', async (req, res) => {
   try {
-    const games = db.collection("game_collection");
+    const games = db.collection('game_collection');
 
-    const users = await db.collection("user");
+    const users = await db.collection('user');
 
     await games.updateOne(
       { _id: ObjectId(req.body.like) },
@@ -96,55 +94,53 @@ app.post("/", async (req, res) => {
     );
 
     await users.updateOne(
-      { _id: ObjectId("62967307a4ff59e9678d2943") },
-      { $push: { game_id: req.body.like } }
+      { _id: ObjectId('62967307a4ff59e9678d2943') },
+      { $addToSet: { game_id: req.body.like } }
     );
 
-    res.redirect("/");
+    res.redirect('/');
   } catch (err) {
     console.log(err);
   }
 });
 
 // unlike
-app.post("/unlike", async (req, res) => {
-  try {
-    const games = db.collection("game_collection");
+app.post('/unlike', async (req, res) => {
+  const games = db.collection('game_collection');
 
-    const users = await db.collection("user");
+  const users = await db.collection('user');
 
-    await games.updateOne(
-      { _id: ObjectId(req.body.unlike) },
-      { $set: { like: false } }
-    );
+  await games.updateOne(
+    { _id: ObjectId(req.body.unlike) },
+    { $set: { like: false } }
+  );
 
-    await users.updateOne(
-      { _id: ObjectId("62967307a4ff59e9678d2943") },
-      { $pull: { game_id: req.body.unlike } }
-    );
+  await users.updateOne(
+    { _id: ObjectId('62967307a4ff59e9678d2943') },
+    { $pull: { game_id: req.body.unlike } }
+  );
 
-    res.redirect("/mygames");
-  } catch (err) {
-    console.log(err);
-  }
+  res.redirect('/mygames');
 });
 
 //work in progress
-// mygames display 
-app.get("pages/mygames", async (req, res) => {
-  const user = await db.collection("user").findOne("62967307a4ff59e9678d2943");
-  const games = await db.collection("game_collection").find({}, {}).toArray();
+// mygames display
+app.get('/mygames', async (req, res) => {
+  const user = await db.collection('user').findOne({
+    _id: ObjectId('62967307a4ff59e9678d2943'),
+  });
+  const games = await db.collection('game_collection').find({}, {}).toArray();
+
+  const userGames = games.filter((game) =>
+    user.game_id.includes(String(game._id))
+  );
+  // console.log(userGames);
   console.log(user);
-  const userGames = games.filter((game) => user.game_id.includes(game.id));
-  console.log(userGames);
 
-  res.render("pages/mygames", { user, games, userGames });
+  res.render('pages/mygames', { userGames, user });
 });
-
-
-
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).render("pages/404");
+  res.status(404).render('pages/404');
 });
